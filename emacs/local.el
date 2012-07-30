c42d9843b3006124dcfde5145d614e7df0ee3b9e
Jan Riethmayer <jan@riethmayer.de>
2012-07-30 13:21:39 +0200

    cleanup dotfiles

--- emacs/local.el
+++
@@ -1,9 +0,0 @@
-;; this file is intended to have changes for current machine only.
-(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
-(add-to-list 'load-path dotfiles-dir)
-
-(load "defunkt")
-(server-start)
-
-(setq initial-frame-alist
-      '((top . 3) (left . 3) (width . 120) (height . 60)))
