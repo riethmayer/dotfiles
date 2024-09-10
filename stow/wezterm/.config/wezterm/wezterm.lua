314f19194e686fb4a935767a117a0a662a20c0d1
Jan Riethmayer <jan@riethmayer.de>
2024-09-10 11:46:21 +0100

    Back to Hack font

--- stow/wezterm/.config/wezterm/wezterm.lua
+++ stow/wezterm/.config/wezterm/wezterm.lua
@@ -3,7 +3,8 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.color_scheme = "Dracula (Official)"
-config.font = wezterm.font("GeistMono Nerd Font Propo")
+-- config.font = wezterm.font("GeistMono Nerd Font Propo")
+config.font = wezterm.font("Hack Nerd Font Mono")
 -- =>
 config.font_size = 24.0
 return config
