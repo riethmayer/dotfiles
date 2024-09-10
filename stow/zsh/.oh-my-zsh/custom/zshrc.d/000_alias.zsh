bcaa5c38ad5c8864a5ad33cb02b1c709158adee8
Jan Riethmayer <jan@riethmayer.de>
2024-09-10 11:46:21 +0100

    Back to Hack font

--- stow/zsh/.oh-my-zsh/custom/zshrc.d/000_alias.zsh
+++ stow/zsh/.oh-my-zsh/custom/zshrc.d/000_alias.zsh
@@ -5,12 +5,15 @@ alias vim=nvim
 alias tg=terragrunt
 alias t=tmux
 
-export OBSIDIAN_VAULT="$HOME/obsidian/riethmayer"
 # Obsidian
+export OBSIDIAN_VAULT="$HOME/obsidian/riethmayer"
 alias oo='cd $OBSIDIAN_VAULT'
 alias or='nvim $OBSIDIAN_VAULT/inbox/*.md'
 
 # Syntax highlighting with Chroma
-
 export LESSOPEN='| p() { chroma --fail "$1" || cat "$1"; }; p "%s"'
+
+# Fonts
+
+alias nerdfonts='fc-list : family | grep Nerd'
 
