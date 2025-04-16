383e65cda2be4a9af56375890e31a5416e57cc39
Jan Riethmayer <jan@riethmayer.de>
2025-04-16 21:14:03 +0100

    Refactor Node.js configuration in Zsh
    - Removed Volta installation checks and exports from the Zsh configuration file.
    - Simplified the setup by eliminating unnecessary commands related to Volta, Node.js, and pnpm.
    - Retained Bun installation configuration.

--- stow/zsh/.oh-my-zsh/custom/zshrc.d/050_node.zsh
+++ stow/zsh/.oh-my-zsh/custom/zshrc.d/050_node.zsh
@@ -1,26 +1,5 @@
 # Javascript/Typescript/Node configuration
 
-export VOLTA_HOME=$HOME/.volta
-export PATH=$VOLTA_HOME/bin:$PATH
-
-# if volta is not installed
-if ! command -v volta &> /dev/null; then
-  echo "Installing volta..."
-  /bin/bash -c "$(curl https://get.volta.sh) -s -- skip-setup"
-fi
-
-if ! command -v node &> /dev/null; then
-  echo "Installing node..."
-  volta install node
-fi
-
-export VOLTA_FEATURE_PNPM=1
-
-if ! command -v pnpm &> /dev/null; then
-  echo "Installing pnpm..."
-  volta install pnpm
-fi
-
 # Bun
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
