acd981e8613bfe112f71b4a3b5f3920ea792b25b
Jan Riethmayer <jan@earlybird.com>
2026-01-04 17:18:50 +0100

    mise/go: update versions, unset GOBIN for consistent install

--- stow/zsh/.config/zsh/052_golang.zsh
+++ stow/zsh/.config/zsh/052_golang.zsh
@@ -3,5 +3,7 @@
 
 if command -v go &> /dev/null; then
     export GOPATH="$(go env GOPATH)"
+    # Unset GOBIN so go install uses $GOPATH/bin consistently
+    unset GOBIN
     export PATH="${PATH}:${GOPATH}/bin"
 fi
