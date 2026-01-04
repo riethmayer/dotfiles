6b3ce8c861b6254add5859e63d6b3d81d35b439f
Jan Riethmayer <jan@earlybird.com>
2026-01-04 19:11:14 +0100

    nvim: fix LazyExtras warnings, add lang.yaml

--- stow/nvim/.config/nvim/lua/plugins/extras.lua
+++ stow/nvim/.config/nvim/lua/plugins/extras.lua
@@ -1,45 +1,11 @@
 return {
-  -- Dracula theme
-  { "Mofiqul/dracula.nvim", 
-    priority = 1000, 
+  -- Dracula theme (override LazyVim default tokyonight)
+  {
+    "Mofiqul/dracula.nvim",
+    priority = 1000,
     config = function()
       require("lazy.core.loader").disable_rtp_plugin("tokyonight.nvim")
       vim.cmd([[colorscheme dracula]])
-    end 
+    end,
   },
-
-  -- FZF integration
-  { 
-    "ibhagwan/fzf-lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    keys = {
-      { "<leader>f", "<cmd>FzfLua files<cr>", desc = "Find Files" },
-      { "<leader>g", "<cmd>FzfLua live_grep<cr>", desc = "Live Grep" },
-    },
-    config = function()
-      require("fzf-lua").setup({
-        winopts = {
-          height = 0.4,
-          width = 0.8,
-          preview = { hidden = "hidden" }
-        }
-      })
-    end
-  },
-
-  -- Clipboard support
-  { 
-    "gbprod/yanky.nvim",
-    dependencies = { "kkharji/sqlite.lua" },
-    opts = {
-      highlight = { timer = 250 },
-      ring = { storage = "sqlite" }
-    },
-    keys = {
-      { "p", "<Plug>(yanky-p)", mode = { "n", "x" } },
-      { "P", "<Plug>(yanky-P)", mode = { "n", "x" } },
-      { "<c-n>", "<Plug>(yanky-cycle-forward)" },
-      { "<c-p>", "<Plug>(yanky-cycle-backward)" },
-    }
-  }
 }
