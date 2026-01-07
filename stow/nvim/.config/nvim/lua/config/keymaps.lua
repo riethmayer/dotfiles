eaa69d6122a3fe952d8fb90440416942a92bbd4a
Jan Riethmayer <jan@riethmayer.de>
2026-01-07 13:38:00 +0100

    nvim: F5/F6 fzf keymaps, disable neo-tree for snacks.explorer

--- stow/nvim/.config/nvim/lua/config/keymaps.lua
+++ stow/nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- F5: find files, F6: grep
+map("n", "<F5>", "<cmd>FzfLua files<cr>", { desc = "Find Files" })
+map("n", "<F6>", "<cmd>FzfLua live_grep<cr>", { desc = "Grep" })
