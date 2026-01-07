2e1b2a3706c70579ee47293ee843056e1c7f336a
Jan Riethmayer <jan@riethmayer.de>
2026-01-07 13:38:00 +0100

    nvim: F5/F6 fzf keymaps, disable neo-tree for snacks.explorer

--- stow/nvim/.config/nvim/lua/plugins/extras.lua
+++ stow/nvim/.config/nvim/lua/plugins/extras.lua
@@ -8,9 +8,14 @@ return {
       vim.cmd([[colorscheme dracula]])
     end,
   },
+  -- Disable neo-tree (using snacks.explorer instead)
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   -- Show hidden/ignored files in explorer by default
   {
     "folke/snacks.nvim",
+    keys = {
+      { "<leader>e", function() Snacks.explorer() end, desc = "Explorer" },
+    },
     opts = {
       explorer = {
         replace_netrw = true,
