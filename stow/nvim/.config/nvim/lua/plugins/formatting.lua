7303532eb8c07ff35ae4d1d552da8dc814986820
Jan Riethmayer <jan@riethmayer.de>
2026-01-07 12:54:25 +0100

    fix conform.nvim: remove format_on_save, let LazyVim handle

--- stow/nvim/.config/nvim/lua/plugins/formatting.lua
+++ stow/nvim/.config/nvim/lua/plugins/formatting.lua
@@ -2,18 +2,14 @@ return {
   -- Auto-format on save with conform.nvim (LazyVim default)
   {
     "stevearc/conform.nvim",
-    opts = function(_, opts)
-      opts.formatters_by_ft = opts.formatters_by_ft or {}
-      opts.formatters_by_ft.python = { "ruff_format", "ruff_organize_imports" }
-      opts.formatters_by_ft.markdown = { "prettierd", "prettier", stop_after_first = true }
-      opts.formatters_by_ft.go = { "gofumpt", "goimports" }
-      opts.formatters_by_ft.fish = {} -- disable fish (not used)
-      opts.format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      }
-      return opts
-    end,
+    opts = {
+      formatters_by_ft = {
+        python = { "ruff_format", "ruff_organize_imports" },
+        markdown = { "prettierd", "prettier", stop_after_first = true },
+        go = { "gofumpt", "goimports" },
+        fish = {}, -- disable fish (not used)
+      },
+    },
   },
 
   -- Ensure formatters are installed
